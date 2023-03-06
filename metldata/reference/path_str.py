9330e8eeb11ae271b6f4188369b22847f35826b4
KerstenBreuer <kersten-breuer@outlook.com>
2023-03-06 15:23:39 +0000

    include suggestions

--- metldata/reference/path_str.py
+++ metldata/reference/path_str.py
@@ -29,7 +29,7 @@ ACTIVE_ARROW_PATTERN = rf"\({NAME_PATTERN}\)>"
 PASSIVE_ARROW_PATTERN = rf"<\({NAME_PATTERN}\)"
 ARROW_PATTERN = rf"(({ACTIVE_ARROW_PATTERN})|({PASSIVE_ARROW_PATTERN}))"
 ELEMENT_PATTERN = rf"{NAME_PATTERN}{ARROW_PATTERN}{NAME_PATTERN}"
-PATH_RAW_CHAR_PATTERN = r"^[\w \n><\(\)]+$"
+PATH_RAW_CHAR_PATTERN = r"^[\w><\(\)]+$"
 PATH_PATTERN = (
     rf"^{NAME_PATTERN}{ARROW_PATTERN}({NAME_PATTERN}{ARROW_PATTERN})*{NAME_PATTERN}$"
 )
@@ -39,8 +39,15 @@ class ValidationError(RuntimeError):
     """Raised when a path string was invalid"""
 
 
+def clean_path_str(path_str: str) -> str:
+    """Cleanup whitespaces, newlines, etc."""
+
+    return "".join(path_str.split())
+
+
 def validate_path_str_characters(path_str: str) -> None:
-    """Validates the characters of the an uncleaned path str.
+    """Validates the characters of the an uncleaned path str. The path_str is assumed to
+    be cleaned.
 
     Raises:
         ValidationError: if invalid.
@@ -53,7 +60,7 @@ def validate_path_str_characters(path_str: str) -> None:
 
 
 def validate_path_str_format(path_str: str) -> None:
-    """Validates the format of the path str.
+    """Validates the format of the path str. The path_str is assumed to be cleaned.
 
     Raises:
         ValidationError: if invalid.
@@ -66,7 +73,8 @@ def validate_path_str_format(path_str: str) -> None:
 
 
 def validate_string_element(string_element: str) -> None:
-    """Validates the format of a string-based path element.
+    """Validates the format of a string-based path element. The path_str is assumed to
+    be cleaned.
 
     Raises:
         ValidationError: if invalid.
@@ -79,14 +87,8 @@ def validate_string_element(string_element: str) -> None:
         )
 
 
-def clean_path_str(path_str: str) -> str:
-    """Cleanup whitespaces, newlines, etc."""
-
-    return "".join(path_str.split())
-
-
 def extract_first_element(*, path_str: str) -> str:
-    """Extract the first element of a path_str.
+    """Extract the first element of a path_str. The path_str is assumed to be cleaned.
 
     Raises:
         ValidationError: if no element can be extracted.
@@ -101,7 +103,7 @@ def extract_first_element(*, path_str: str) -> str:
 
 
 def get_target_class(*, path_str: str) -> str:
-    """Get the target class of a path str."""
+    """Get the target class of a path str. The path_str is assumed to be cleaned."""
 
     match = re.match(rf"^.*?({NAME_PATTERN})$", path_str)
 
@@ -116,6 +118,7 @@ def split_first_element(*, path_str: str) -> tuple[str, Optional[str]]:
     Thereby, the target class of the first element is set as the source class of the
     remaining path.
     The second element is None if the provided path only contained one element.
+    The path_str is assumed to be cleaned.
     """
 
     first_element = extract_first_element(path_str=path_str)
@@ -131,7 +134,8 @@ def split_first_element(*, path_str: str) -> tuple[str, Optional[str]]:
 
 
 def get_string_elements(*, path_str: str) -> list[str]:
-    """Decomposes a path string into elements in string repesentation."""
+    """Decomposes a path string into elements in string repesentation. The path_str is
+    assumed to be cleaned."""
 
     elements: list[str] = []
     remaining_path = path_str
@@ -188,7 +192,8 @@ def string_element_to_object(string_element: str) -> ReferencePathElement:
 
 
 def path_str_to_object_elements(path_str: str) -> list[ReferencePathElement]:
-    """Translates a path string into a list of object-based elements."""
+    """Translates a path string into a list of object-based elements. The path_str is
+    assumed to be cleaned."""
 
     validate_path_str_characters(path_str=path_str)
     validate_path_str_format(path_str=path_str)
