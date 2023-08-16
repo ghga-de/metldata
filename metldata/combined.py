869f4d42c48474c67a7f5fbf6d9f2d0dd218901c
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2023-08-16 12:06:34 +0000

    Fix creation of the combined API

--- metldata/combined.py
+++ metldata/combined.py
@@ -41,6 +41,7 @@ async def get_app(config: Config) -> FastAPI:
 
     load_router = await load_rest_api_factory(
         artifact_infos=config.artifact_infos,
+        primary_artifact_name=config.primary_artifact_name,
         config=config,
         dao_factory=dao_factory,
         db_aggregator=db_aggregator,
