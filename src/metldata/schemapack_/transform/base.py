9754505affb8916e8884295d7153cb19fc87da35
Kersten Breuer <kersten-breuer@outlook.com>
2023-12-11 09:04:55 +0100

    Update src/metldata/schemapack_/transform/base.py
    Co-authored-by: Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>

--- src/metldata/schemapack_/transform/base.py
+++ src/metldata/schemapack_/transform/base.py
@@ -105,7 +105,7 @@ class TransformationDefinition(Generic[Config]):
         ...,
         description=(
             "A class for transforming data. Raises a DataTransformationError"
-            "if the transformation fails."
+            " if the transformation fails."
         ),
     )
 
