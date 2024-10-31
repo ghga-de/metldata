6bf5e5963304a3f25171504a5e6644cb1791cf64
sbilge <bilge.sueruen@uni-tuebingen.de>
2024-10-31 12:38:59 +0000

    data transform func is refactored

--- src/metldata/builtin_transformations/count_content_values/data_transform.py
+++ src/metldata/builtin_transformations/count_content_values/data_transform.py
@@ -31,18 +31,36 @@ from metldata.transform.exceptions import (
 )
 
 
+class TransformationContext:
+    """Context manager for a data transformation process, wrapping the Datapack to be
+    transformed and transformation instructions per class.
+    """
+
+    def __init__(
+        self,
+        data: DataPack,
+        instructions_by_class: dict[str, list[CountContentValueInstruction]],
+    ):
+        self.data = data.model_copy(deep=True)
+        self.instructions_by_class = instructions_by_class
+
+    def get_class_resources(self, class_name: str):
+        """Extract the resources of a given class from a Datapack."""
+        resources = self.data.resources.get(class_name)
+        if not resources:
+            raise EvitableTransformationError()
+        return resources
+
+
 def count_content(
     *,
     data: DataPack,
     instructions_by_class: dict[str, list[CountContentValueInstruction]],
 ) -> DataPack:
     """Transforms the data."""
-    modified_data = data.model_copy(deep=True)
-    for class_name, instructions in instructions_by_class.items():
-        resources = modified_data.resources.get(class_name)
-
-        if not resources:
-            raise EvitableTransformationError()
+    context = TransformationContext(data, instructions_by_class)
+    for class_name, instructions in context.instructions_by_class.items():
+        resources = context.get_class_resources(class_name)
 
         for instruction in instructions:
             relation_path = instruction.source.relation_path
@@ -50,40 +68,63 @@ def count_content(
 
             # Only one element is expected in the path
             relation_name = relation_path.elements[0].property
+            # content_resources keeping the values to be count are obtained from
+            # the class that is referenced by the transformation target class through
+            # relation_name.
+            content_resources = context.get_class_resources(referenced_class)
 
-            content_resources = modified_data.resources.get(referenced_class)
+            transform_resources(
+                resources, relation_name, content_resources, instruction
+            )
 
-            if not content_resources:
-                raise EvitableTransformationError()
+    return context.data
 
-            for resource in resources.values():
-                content = resource.content
-                related_to = resource.relations.get(relation_name)
-                if not related_to:
-                    raise EvitableTransformationError()
 
-                try:
-                    count_values = [
-                        content_resources[relation].content.get(
-                            instruction.source.content_path
-                        )
-                        for relation in related_to
-                    ]
-                except KeyError as exc:
-                    raise EvitableTransformationError() from exc
+def transform_resources(
+    resources,
+    relation_name,
+    content_resources,
+    instruction: CountContentValueInstruction,
+):
+    """Transform resources"""
+    for resource in resources.values():
+        content = resource.content
+        related_to = resource.relations.get(relation_name)
+        count_values = get_count_values(related_to, content_resources, instruction)
 
-                object = resolve_data_object_path(
-                    data=content,
-                    path=instruction.target_content.object_path,
-                )
-                if (
-                    not isinstance(object, dict)
-                    or instruction.target_content.property_name in object
-                ):
-                    raise EvitableTransformationError()
+        target_object = get_target_object(content, instruction)
+        target_object[instruction.target_content.property_name] = dict(
+            Counter(count_values)
+        )
 
-                object[instruction.target_content.property_name] = dict(
-                    Counter(count_values)
-                )
 
-    return modified_data
+def get_count_values(
+    resource_relations, content_resources, instruction: CountContentValueInstruction
+):
+    """Get values that is to be counted."""
+    if not resource_relations:
+        raise EvitableTransformationError()
+
+    try:
+        count_values = [
+            content_resources[relation].content.get(instruction.source.content_path)
+            for relation in resource_relations
+        ]
+        return count_values
+    except KeyError as exc:
+        raise EvitableTransformationError() from exc
+
+
+def get_target_object(content, instruction: CountContentValueInstruction):
+    """Get the json object that is to be modify."""
+    target_object = resolve_data_object_path(
+        data=content,
+        path=instruction.target_content.object_path,
+    )
+    if (
+        not isinstance(target_object, dict)
+        or instruction.target_content.property_name in target_object
+    ):
+        raise EvitableTransformationError()
+
+    return target_object
