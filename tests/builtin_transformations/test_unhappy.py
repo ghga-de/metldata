e0f7e9ab544fd48aa1d32e2cc6eb470b90e30d5d
Thomas J. Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2025-02-07 14:05:09 +0000

    Refactoring to enable unhappy test cases

--- tests/builtin_transformations/test_unhappy.py
+++ tests/builtin_transformations/test_unhappy.py
@@ -1,4 +1,4 @@
-# Copyright 2021 - 2025 Universität Tübingen, DKFZ, EMBL, and Universität zu Köln
+# Copyright 2021 - 2024 Universität Tübingen, DKFZ, EMBL, and Universität zu Köln
 # for the German Human Genome-Phenome Archive (GHGA)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,40 +16,56 @@
 
 """Test the builtin transformations using pre-defined test cases."""
 
+from contextlib import nullcontext
+
 import pytest
 
 from metldata.transform.handling import TransformationHandler
 from tests.fixtures.transformations import (
-    HAPPY_TEST_CASES,
+    UNHAPPY_TEST_CASES,
     TransformationTestCase,
 )
 
+# for now only holds dummy values
+# needs to be adjusted for actual test cases
+DATA_TRANSFORMATION_ERRORS = {"dummy_case-single": Exception}
+MODEL_TRANSFORMATION_ERRORS = {"dummy_case-single": Exception}
 
-@pytest.mark.parametrize("test_case", HAPPY_TEST_CASES, ids=str)
+
+@pytest.mark.parametrize("test_case", UNHAPPY_TEST_CASES, ids=str)
 def test_model_transformations(
+    request,
     test_case: TransformationTestCase,
 ):
     """Test the happy path of transforming a model."""
-    handler = TransformationHandler(
-        transformation_definition=test_case.transformation_definition,
-        transformation_config=test_case.config,
-        input_model=test_case.input_model,
-    )
+    test_id = request.node.callspec.id
+    expected_exception = MODEL_TRANSFORMATION_ERRORS.get(test_id)
+    with pytest.raises(expected_exception) if expected_exception else nullcontext():
+        handler = TransformationHandler(
+            transformation_definition=test_case.transformation_definition,
+            transformation_config=test_case.config,
+            input_model=test_case.input_model,
+        )
     transformed_model = handler.transformed_model
 
     assert transformed_model == test_case.transformed_model
 
 
-@pytest.mark.parametrize("test_case", HAPPY_TEST_CASES, ids=str)
+@pytest.mark.parametrize("test_case", UNHAPPY_TEST_CASES, ids=str)
 def test_data_transformations(
+    request,
     test_case: TransformationTestCase,
 ):
     """Test the happy path of transforming data for a model."""
+    # fetch test ID from FixtureRequest to get exception from map
+    test_id = request.node.callspec.id
+    expected_exception = MODEL_TRANSFORMATION_ERRORS.get(test_id)
     handler = TransformationHandler(
         transformation_definition=test_case.transformation_definition,
         transformation_config=test_case.config,
         input_model=test_case.input_model,
     )
-    transformed_data = handler.transform_data(test_case.input_data)
+    with pytest.raises(expected_exception) if expected_exception else nullcontext():
+        transformed_data = handler.transform_data(test_case.input_data)
 
     assert transformed_data == test_case.transformed_data
