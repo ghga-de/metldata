62b3116fb0193a063435ecbe94d6436d116544d9
Thomas J. Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2025-11-28 10:55:10 +0000

    Correct test cases

--- tests/workflow/test_validation.py
+++ tests/workflow/test_validation.py
@@ -15,37 +15,65 @@
 
 """Test workflow execution using pre-defined test cases."""
 
-from unittest.mock import patch
-
 import pytest
-from schemapack.spec.schemapack import SchemaPack
 
-from metldata.transform.base import Config, TransformationDefinition
 from metldata.transform.exceptions import (
     PostTransformValidationError,
     PreTransformValidationError,
 )
-from metldata.transform.handling import NoOpValidator, TransformationHandler
+from metldata.transform.handling import TransformationHandler
 from metldata.workflow.handling import WorkflowHandler
 from tests.fixtures.workflow import VALIDATION_TEST_CASES, WorkflowTestCase
 
+EXPECTED_CALL_ARGS = {
+    "invalid_input_model": [{"validate_input": True, "validate_output": False}],
+    "invalid_intermediate_model": [
+        {"validate_input": True, "validate_output": False},
+        {"validate_input": False, "validate_output": False},
+        {"validate_input": False, "validate_output": True},
+    ],
+    "invalid_single_step_model": [{"validate_input": True, "validate_output": True}],
+    "invalid_output_model": [
+        {"validate_input": True, "validate_output": False},
+        {"validate_input": False, "validate_output": False},
+        {"validate_input": False, "validate_output": True},
+    ],
+}
 
-def constructor_intercept(
-    self,
-    transformation_definition: TransformationDefinition[Config],
-    transformation_config: Config,
-    input_model: SchemaPack,
-    validate_input: bool = False,
-    validate_output: bool = False,
-):
-    pass
+
+class CallArgs:
+    """Container to store results from capture_constructor_args"""
+
+    def __init__(self):
+        self.call_args = []
+
+
+@pytest.fixture()
+def capture_constructor_args():
+    """Capture call args for TransformationHandler constructor."""
+    call_args = CallArgs()
+
+    def capture(self, *args, **kwargs):
+        call_args.call_args.append(
+            {
+                "validate_input": kwargs["validate_input"],
+                "validate_output": kwargs["validate_output"],
+            }
+        )
+        self._real_init(*args, **kwargs)
+
+    TransformationHandler._real_init = TransformationHandler.__init__  # type: ignore
+    TransformationHandler.__init__ = capture
+
+    yield call_args
+
+    TransformationHandler.__init__ = TransformationHandler._real_init
+    del TransformationHandler._real_init  # type: ignore
 
 
 @pytest.mark.parametrize("test_case", VALIDATION_TEST_CASES, ids=str)
-def test_workflow_invalid_models(test_case: WorkflowTestCase):
+def test_workflow_invalid_models(test_case: WorkflowTestCase, capture_constructor_args):
     """Test validation using an invalid input, intermediate or output model."""
-    # patch("metldata.transform.handling.TransformationHandler.__init__", constructor_intercept)
-
     handler: WorkflowHandler = WorkflowHandler(
         workflow=test_case.workflow,
         transformation_registry=test_case.transformation_registry,
@@ -58,3 +86,5 @@ def test_workflow_invalid_models(test_case: WorkflowTestCase):
     )
     with pytest.raises(exception_type):
         _ = handler.run(data=test_case.input_data, annotation=test_case.annotation)
+
+    assert capture_constructor_args.call_args == EXPECTED_CALL_ARGS[test_case.case_name]
